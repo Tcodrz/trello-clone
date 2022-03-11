cbdf2cd858808bc48c5469e11dd3068ab56ca1d6
ttomz <ttomz4291@gmail.com>
2022-03-11 22:26:31 +0200

    layout

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { LayoutModule } from './../layout/layout.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
@@ -9,7 +10,8 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
   ]
 })
 export class LoginModule { }
