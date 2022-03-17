00789b27b2c21057b55f9abe36c271cc19b27c23
ttomz <ttomz4291@gmail.com>
2022-03-18 00:39:19 +0200

    layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,26 +1,35 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ButtonComponent } from './button/button.component';
 import { CardComponent } from './card/card.component';
 import { IconComponent } from './icon/icon.component';
-import { ButtonComponent } from './button/button.component';
+import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 
 
 @NgModule({
   declarations: [
+    ButtonComponent,
     CardComponent,
     IconComponent,
-    ButtonComponent,
+    LayoutComponent,
+    SidebarComponent,
+    TopnavComponent,
   ],
   imports: [
     CommonModule,
     FontAwesomeModule,
+    RouterModule,
   ],
   exports: [
+    ButtonComponent,
     CardComponent,
     IconComponent,
-    ButtonComponent,
+    LayoutComponent,
   ]
 })
 export class LayoutModule { }
