361770fa2ea5ed3518eb18b96735fde000b42b4a
ttomz <ttomz4291@gmail.com>
2022-03-18 18:38:15 +0200

    layout sidebar toggle

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
+  @ViewChild('sidebar') sidebar?: TemplateRef<SidebarComponent>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSidebarToggle() {
+    this.sidebar?.elementRef.nativeElement.classList.toggle('closed');
   }
 
 }
