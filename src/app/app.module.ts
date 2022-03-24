a194f717b1549ddb716a4d6a2036aefc7c6b68c1
ttomz <ttomz4291@gmail.com>
2022-03-24 22:33:04 +0200

    profile menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 // AngularFire Docs - https://www.npmjs.com/package/@angular/fire
 
@@ -17,9 +16,9 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireStorageModule,
   ],
   providers: [
   ],
