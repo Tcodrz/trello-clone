93087b4cbe2ac66aa33ff26c74d5a365de9d52c6
ttomz <ttomz4291@gmail.com>
2022-03-28 19:02:22 +0300

    CardComponent

--- src/app/ui-components/card/card.component.ts
+++ src/app/ui-components/card/card.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent implements OnInit {
-
+  @Input() className: string = '';
   constructor() { }
 
   ngOnInit(): void {
