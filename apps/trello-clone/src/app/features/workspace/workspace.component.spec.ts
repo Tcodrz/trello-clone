60a3d1e3a1a2e2fa36d5e0d5ec51fbe914f1460b
tomzo42 <tom.zohar@hibob.io>
2022-05-26 22:40:33 +0300

    tests/workspace component

--- apps/trello-clone/src/app/features/workspace/workspace.component.spec.ts
+++ apps/trello-clone/src/app/features/workspace/workspace.component.spec.ts
@@ -1,25 +1,108 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {WorkspaceComponent} from './workspace.component';
+import {createBoardsServiceMock} from "../../mocks/board.service.mock";
+import {BoardsService} from "../../core/services/boards.service";
+import {GotoService} from "../../core/services/goto.service";
+import {WorkspaceService} from "../../core/services/workspace.service";
+import {createGotoServiceMock} from "../../mocks/goto.service.mock";
+import {createWorkspaceServiceMock, MOCK_WORKSPACE} from "../../mocks/workspace.service.mock";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {of} from "rxjs";
+import {Board} from "../../core/interface/board.interface";
+import {Theme} from "../../core/interface/themes";
 
-import { WorkspaceComponent } from './workspace.component';
+
+function createActivatedRouteMock(): Partial<ActivatedRoute> {
+  return {
+    params: of({'workspaceID': MOCK_WORKSPACE.id} as unknown as ParamMap),
+  };
+}
+
+const MOCK_BOARD: Board = {
+  createdAt: 0,
+  members: [],
+  name: 'mock board',
+  theme: {} as Theme,
+  updatedAt: 0,
+  id: '1',
+  lists: [],
+  listIDs: ['1'],
+  workspaceID: '1'
+};
 
 describe('WorkspaceComponent', () => {
   let component: WorkspaceComponent;
   let fixture: ComponentFixture<WorkspaceComponent>;
+  let boardsServiceMock: BoardsService;
+  let gotoServiceMock: GotoService;
+  let workspaceServiceMock: WorkspaceService;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WorkspaceComponent ]
+      declarations: [WorkspaceComponent],
+      providers: [
+        {provide: ActivatedRoute, useValue: createActivatedRouteMock()},
+        {provide: BoardsService, useValue: createBoardsServiceMock()},
+        {provide: GotoService, useValue: createGotoServiceMock()},
+        {provide: WorkspaceService, useValue: createWorkspaceServiceMock()}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WorkspaceComponent);
     component = fixture.componentInstance;
+    boardsServiceMock = TestBed.inject(BoardsService);
+    gotoServiceMock = TestBed.inject(GotoService);
+    workspaceServiceMock = TestBed.inject(WorkspaceService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () =>{
+    beforeEach(() => {
+      component.ngOnInit();
+    });
+
+    it('Should initialize view$', () => {
+      expect(component.view$).toBeTruthy();
+    });
+
+    it('Should Call workspaceService.getWorkspace with workspace id', () => {
+      expect(workspaceServiceMock.getWorkspace).toHaveBeenCalledWith(MOCK_WORKSPACE.id);
+      component.view$.subscribe((view) => {
+          expect(view.currentWorkspace).toEqual(MOCK_WORKSPACE);
+      });
+    });
+
+    it('Should call workspaceService.getAll', () => {
+      expect(workspaceServiceMock.getAll).toHaveBeenCalled();
+    });
+
+    it('Should call boardsService.getBoards with workspace id', () => {
+      expect(boardsServiceMock.getBoards).toHaveBeenCalledWith(MOCK_WORKSPACE.id);
+    });
+  });
+
+  describe('onBoardClick', () => {
+    it('Should call gotoService.board with board id and workspaceID', () => {
+        component.onBoardClick(MOCK_BOARD);
+        expect(gotoServiceMock.board).toHaveBeenCalledWith(MOCK_BOARD.id, MOCK_BOARD.workspaceID)
+    });
+  });
+
+  describe('onCreateBoard', () => {
+    beforeEach(() => {
+      component.onCreateBoard(MOCK_BOARD);
+    });
+
+    it('Should call boardsService.createNewBoard with new board', () => {
+        expect(boardsServiceMock.createNewBoard).toHaveBeenCalledWith(MOCK_BOARD);
+    });
   });
 });
--- apps/trello-clone/src/app/features/workspace/workspace.component.ts
+++ apps/trello-clone/src/app/features/workspace/workspace.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { combineLatest, Observable } from 'rxjs';
-import { Board } from '../../core/interface/board.interface';
-import { Workspace } from '../../core/interface/workspace.interface';
-import { BoardsService } from './../../core/services/boards.service';
-import { GotoService } from './../../core/services/goto.service';
-import { WorkspaceService } from './../../core/services/workspace.service';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {combineLatest, Observable, switchMap} from 'rxjs';
+import {Board} from '../../core/interface/board.interface';
+import {Workspace} from '../../core/interface/workspace.interface';
+import {BoardsService} from '../../core/services/boards.service';
+import {GotoService} from '../../core/services/goto.service';
+import {WorkspaceService} from '../../core/services/workspace.service';
 
 @Component({
   selector: 'app-workspace',
@@ -15,6 +15,7 @@ import { WorkspaceService } from './../../core/services/workspace.service';
 })
 export class WorkspaceComponent implements OnInit {
   view$!: Observable<{ currentWorkspace: Workspace | null, workspaces: Workspace[], boards: Board[] }>;
+
   constructor(
     private activeRoute: ActivatedRoute,
     private boardsService: BoardsService,
@@ -23,17 +24,23 @@ export class WorkspaceComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.activeRoute.params.subscribe((params: any) => {
-      this.view$ = combineLatest({
-        currentWorkspace: this.workspaceService.getWorkspace(params.workspaceID),
-        workspaces: this.workspaceService.getAll(),
-        boards: this.boardsService.getBoards(params.workspaceID),
-      });
-    });
+    this.view$ = this.activeRoute.params
+      .pipe(
+        switchMap((params: Params) => {
+            return combineLatest({
+              currentWorkspace: this.workspaceService.getWorkspace(params['workspaceID']),
+              workspaces: this.workspaceService.getAll(),
+              boards: this.boardsService.getBoards(params['workspaceID']),
+            })
+          }
+        )
+      );
   }
+
   onBoardClick(board: Board) {
     this.goto.board(board.id, board.workspaceID);
   }
+
   onCreateBoard(board: Partial<Board>) {
     this.boardsService.createNewBoard(board);
   }
