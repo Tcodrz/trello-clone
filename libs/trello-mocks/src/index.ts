191d49bbc4a0ec4961bd8fbc46fc6cf2ae58558f
tcodrz <ttomz4291@gmail.com>
2022-05-27 14:14:18 +0300

    generate libs for interface and mocks

---
+++ libs/trello-mocks/src/index.ts
@@ -0,0 +1,7 @@
+export * from './mocks/active-route.mock';
+export * from './mocks/board.service.mock';
+export * from './mocks/cache.service.mock';
+export * from './mocks/goto.service.mock';
+export * from './mocks/sidebar.service.mock';
+export * from './mocks/user.service.mock';
+export * from './mocks/workspace.service.mock';
