269c74eb8c53689761eedec54746df5fc1b028f4
tcodrz <ttomz4291@gmail.com>
2022-05-28 12:43:27 +0300

    generate libs for interface and mocks

--- apps/trello-clone/src/app/features/topnav/topnav.module.ts
+++ apps/trello-clone/src/app/features/topnav/topnav.module.ts
@@ -1,24 +1,14 @@
-import {ButtonModule, MenuModule} from '@ui-components';
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {ProfileMenuComponent} from './profile-menu/profile-menu.component';
-import {TopnavComponent} from './topnav.component';
+import { ButtonModule, MenuModule } from '@ui-components';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
+import { TopnavComponent } from './topnav.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
-  declarations: [
-    ProfileMenuComponent,
-    TopnavComponent,
-  ],
-  imports: [
-    ButtonModule,
-    CommonModule,
-    MenuModule,
-    RouterModule,
-  ],
-  exports: [
-    TopnavComponent,
-  ]
+  declarations: [ProfileMenuComponent, TopnavComponent, LogoComponent],
+  imports: [ButtonModule, CommonModule, MenuModule, RouterModule],
+  exports: [TopnavComponent],
 })
-export class TopnavModule {
-}
+export class TopnavModule {}
